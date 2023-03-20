814752c95d07a052a30f9fe2135a54d787a47f98
fathimathhasna <115775682+fathimathhasna@users.noreply.github.com>
2023-03-20 16:25:15 +0530

    Add files via upload

---
+++ 21 HASNA/01 product/product.java
@@ -0,0 +1,21 @@
+package packhasna;
+
+public class product {
+     String pname,pcode;
+     int price;
+     public product() {}
+     public product(String pname,String pcode,int price) {
+    	 this.pname = pname;
+    	 this.pname = pcode;
+    	 this.price=price; }
+     public int getPrice() {
+    	 return price; }
+    	 public void setPrice(int price) {
+    		 this.price = price; }
+    	public void display() {
+    		System.out.println("pcode:"+this.pcode);
+    		System.out.println("pname:"+this.pname);
+    		System.out.println("price:"+this.price+"\n");
+    	 }
+     }
+
