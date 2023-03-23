65a97a3c82472ebd54627b4ee99a1f0a10dcf916
kenzazain <115775741+kenzazain@users.noreply.github.com>
2023-03-23 16:09:01 +0530

    Add files via upload

---
+++ 33_KHANSA CZ/01_PRODUCT/Product.java
@@ -0,0 +1,24 @@
+package packA;
+
+public class Product {
+	String pname,pcode;
+	int price;
+	public Product() {}
+	public Product(String pname,String pcode,int price) {
+		this.pname=pname;
+		this.pcode=pcode;
+		this.price=price;}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price=price;
+	}
+	public void display() {
+		System.out.println("pcode:"+this.pcode);
+		System.out.println("pname:"+this.pname);
+		System.out.println("price:"+this.price+"\n");
+		
+	}
+
+}
