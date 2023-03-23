79fd028deca39270001d78028f9b8a521b073894
roshnipp111 <77226843+roshnipp111@users.noreply.github.com>
2023-03-23 14:31:43 +0530

    Add files via upload

---
+++ 45 ROSHNI PP/01 PRODUCT/Product.java
@@ -0,0 +1,25 @@
+package packA;
+
+public class Product {
+	String pname,pcode ;
+	int price;
+	public Product() {}
+	public Product(String pname, String pcode, int price) {
+		this.pname = pname;
+		this.pcode = pcode;
+		this.price = price; }
+	public int getPrice() {
+		return price ; }
+	public void setPrice(int price) {
+		this.price = price ;}
+	public void setPname(String pname) {
+		this.pname = pname; }
+	public String getPname() {
+		return pname ;}
+	public String getPcode() {
+		return pcode; }
+	public void display() {
+		System.out.println("pcode :" + this.pcode);
+		System.out.println("pname :" + this.pname);
+		System.out.println("price :"+ this.price + "\n") ;	}
+}
