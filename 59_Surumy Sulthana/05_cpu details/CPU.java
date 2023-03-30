c561496ecdc0921e96dc3876289caef34b4a5b0e
surumysulthana <115775697+surumysulthana@users.noreply.github.com>
2023-03-30 02:52:12 -0700

    Add files via upload

---
+++ 59_Surumy Sulthana/05_cpu details/CPU.java
@@ -0,0 +1,22 @@
+package packSurumy;
+
+public class CPU {
+	double price;
+	class Processor{
+		double cores;
+		String manufacture;
+		double getCache() {
+			return 4.3; 
+		}
+	}
+	static class RAM{
+		double memory;
+		String manufacture;
+		double getClockSpeed() {
+			return 5.5;
+		}
+	}
+	
+}
+
+
---
+++ 59_Surumy Sulthana/05_cpu details/CPUDetails.java
@@ -0,0 +1,20 @@
+package packSurumy;
+
+import packSurumy.CPU.Processor;
+
+public class CPUDetails {
+	public static void main(String[] args) {
+		CPU cpu=new CPU();
+		CPU.Processor processor=cpu.new  Processor ();
+		CPU.RAM ram=new CPU.RAM();
+		System.out.println("processor cache="+processor.getCache());
+		System.out.println("RAM clock speed="+ram.getClockSpeed());
+		
+		
+		
+	}
+		
+
+	}
+
+
