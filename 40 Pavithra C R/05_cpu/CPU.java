42910e4992ba931728e49dfd61b999f822849130
pavithrapavi358 <115776037+pavithrapavi358@users.noreply.github.com>
2023-03-30 16:04:40 +0530

    Add files via upload

---
+++ 40 Pavithra C R/05_cpu/CPU.java
@@ -0,0 +1,18 @@
+package packPavi;
+
+public class CPU {
+	double price;
+	class Processor{
+		double cores;
+		String manufacturer;
+		double getCache() {
+			return 4.3;}}
+	static class RAM{
+		double memory;
+		String manufacturer;
+		double getClockspeed() {
+			return 5.5;}}
+		
+	}
+
+
---
+++ 40 Pavithra C R/05_cpu/CPUDetails.java
@@ -0,0 +1,14 @@
+package packPavi;
+
+public class CPUDetails {
+
+	public static void main(String[] args) {
+	CPU cpu=new CPU();
+	CPU.Processor processor=cpu.new Processor();
+	CPU.RAM ram=new CPU.RAM();
+	System.out.println("processor cache="+processor.getCache());
+	System.out.println("RAM clock="+ram.getClockspeed());
+	
+	}
+
+}
