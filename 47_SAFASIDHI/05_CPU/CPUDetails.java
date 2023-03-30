c4fe04a881c427bdc88c8e05ac0e493e80b872d9
Safa sidhi <115776259+safasidhi@users.noreply.github.com>
2023-03-30 14:42:38 +0500

    Add files via upload

---
+++ 47_SAFASIDHI/05_CPU/CPUDetails.java
@@ -0,0 +1,13 @@
+package packA;
+
+import packA.CPU.Processor;
+
+public class CPUDetails {
+
+	public static void main(String[] args) {
+			   CPU cpu= new CPU();
+			   CPU.Processor processor = cpu.new Processor();
+			   CPU.RAM ram = new CPU.RAM();
+			   System.out.println("Processor Cache = " + processor.getCache());
+			   System.out.println("RAM Clock speed = " + ram.getClockSpeed());}}
+			
