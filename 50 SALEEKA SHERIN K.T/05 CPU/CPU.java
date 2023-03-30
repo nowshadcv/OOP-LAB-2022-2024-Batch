f3592566070d3c7635016aa04616794a51b8b6b0
SALEEKASHERINKT <115775554+SALEEKASHERINKT@users.noreply.github.com>
2023-03-30 06:04:10 -0400

    Add files via upload

---
+++ 50 SALEEKA SHERIN K.T/05 CPU/CPU.java
@@ -0,0 +1,26 @@
+package saleeka;
+
+public class CPU {
+	double price ;
+	class Processor
+	{
+		double cores ;
+		String manufacturer;
+		double getCache ( ) 
+		{
+			return 4.3 ;
+	     }
+	
+	}
+	static class RAM
+	{
+		double memory ;
+		String manufacturer;
+		double getClockspeed() 
+		{
+			return 5.5 ;
+		}
+    }
+}
+
+
---
+++ 50 SALEEKA SHERIN K.T/05 CPU/CPUDetails.java
@@ -0,0 +1,16 @@
+package saleeka;
+
+import saleeka.CPU.Processor;
+
+public class CPUDetails {
+	public static void main(String[] args)
+	{
+		CPU cpu = new CPU() ;
+		CPU.Processor processor = cpu.new Processor() ;
+		CPU.RAM ram = new CPU.RAM();
+		System.out.println("Processor Cache =" + processor.getCache() ) ;
+		System.out.println("RAM Clock speed =" + ram.getClockspeed() ) ;
+		
+	}
+
+}
