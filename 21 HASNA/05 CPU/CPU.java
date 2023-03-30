354aca8db5357e25c8dea0108ace14fdd44481a0
fathimathhasna <115775682+fathimathhasna@users.noreply.github.com>
2023-03-30 15:09:17 +0530

    Add files via upload

---
+++ 21 HASNA/05 CPU/CPU.java
@@ -0,0 +1,17 @@
+package packhasna;
+
+public class CPU {
+	double price;
+	class Processor {
+		double cores;
+		String manufacturer;
+		double getCache() {
+			return 4.3;}}
+	static class RAM{
+		double memory ;
+		String manufacturer;
+		double getClockSpeed() {
+			return 5.5;}}
+		// TODO Auto-generated method stub
+
+	}
---
+++ 21 HASNA/05 CPU/CPUDetails.java
@@ -0,0 +1,15 @@
+package packhasna;
+
+public class CPUDetails {
+
+	public static void main(String[] args) {
+		CPU cpu = new CPU();
+		CPU.Processor processor = cpu.new Processor();
+		CPU.RAM ram = new CPU.RAM();
+		System.out.println("Processor Cache = " + processor.getCache() );
+		System.out.println("RAM Clock speed = " + ram.getClockSpeed() ) ;
+		// TODO Auto-generated method stub
+
+	}
+
+}
