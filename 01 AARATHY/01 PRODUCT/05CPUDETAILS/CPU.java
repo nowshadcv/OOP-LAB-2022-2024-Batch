3487af36524fe82b3a9b17f885e925992ab32501
Arathy07 <115775427+Arathy07@users.noreply.github.com>
2023-03-30 15:54:37 +0530

    Add files via upload

---
+++ 01 AARATHY/01 PRODUCT/05CPUDETAILS/CPU.java
@@ -0,0 +1,25 @@
+package aarathyps;
+
+public class CPU {
+	double price;
+	class processor{
+		double cores;
+		String manufacture;
+		double getCache() {
+			return 4.3;
+		
+			
+		}
+		
+	}
+	static class RAM{
+		double memory;
+		String manufacturer;
+		double getClockSpeed() {
+			return 5.5;
+		}
+		
+	
+	}
+
+}
