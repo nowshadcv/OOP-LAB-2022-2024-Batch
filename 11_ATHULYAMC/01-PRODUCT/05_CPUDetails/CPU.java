7fdb69aa9bfd0a1151b521d44e2aa57e2bfa2efd
AthulyaprasannakumarMC <115775602+AthulyaprasannakumarMC@users.noreply.github.com>
2023-03-30 15:59:17 +0530

    Add files via upload

---
+++ 11_ATHULYAMC/01-PRODUCT/05_CPUDetails/CPU.java
@@ -0,0 +1,19 @@
+package athulyamc;
+
+public class CPU {
+	double price;
+	class processor {
+		double core;
+		String manufacturer;
+		double getcache() {
+			return 4.3;}}
+    static class RAM {
+    	         double memory;
+    	         String manufacturer;
+    	         double getClockSpeed() {
+    	        	 return 5.5;
+    	         
+		}
+	}
+
+}
