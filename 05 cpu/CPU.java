183218de42bc730e0a66cbaeb76bc17039d69868
Rahananinu <115775939+Rahananinu@users.noreply.github.com>
2023-03-30 15:51:30 +0530

    Add files via upload

---
+++ 05 cpu/CPU.java
@@ -0,0 +1,19 @@
+package rahana;
+
+public class CPU {
+
+	public static Processor Processor;
+	double price;
+	class Processor{
+		double cores;
+		String manufacturer;
+		static double getcache() {
+			return 4.3;}}
+static class RAM{
+	double memory;
+	String manufacturer;
+	double getClockSpeed() {
+		return 5.5;
+	}
+}
+}
