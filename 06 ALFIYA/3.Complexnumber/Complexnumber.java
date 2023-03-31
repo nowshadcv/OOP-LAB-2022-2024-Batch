e627ecc74ee21ac6b64f2a02b4a3356d73f4e596
Alfiyafaisalkt <115776061+Alfiyafaisalkt@users.noreply.github.com>
2023-03-31 14:12:34 +0530

    Add files via upload

---
+++ 06 ALFIYA/3.Complexnumber/Complexnumber.java
@@ -0,0 +1,26 @@
+package program3;
+
+public class Complexnumber {
+	double real, img;
+	Complexnumber(double r,double i){
+		real=r; img=i;
+	}
+public static Complexnumber sum(Complexnumber c1,Complexnumber c2){
+	Complexnumber temp=new Complexnumber(0,0);
+	temp.real=c1.real+c2.real;
+	temp.img=c1.img+c2.img;
+	return temp;
+	
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Complexnumber c1=new Complexnumber(5.5,4);
+		Complexnumber c2=new Complexnumber(1.2,3.5);
+		Complexnumber temp=sum(c1,c2);
+		System.out.printf("sum is:"+temp.real+"+" +temp.img+"i");
+		
+
+	}
+}
+
+
