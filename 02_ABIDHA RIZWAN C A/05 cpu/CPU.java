d442aa74409c260f7c2f38182c1ba29a03d8b598
Abidha Rizwan c a <90501016+abidharizwan@users.noreply.github.com>
2023-04-03 02:43:06 -0700

    Add files via upload

---
+++ 02_ABIDHA RIZWAN C A/05 cpu/CPU.java
@@ -0,0 +1,17 @@
+package packA;
+
+public class CPU {
+	double Price;
+	class Processor{
+		double Cores;
+		String manufactures;
+		double getCache() {
+			return 4.3;}}
+	static class RAM{
+		double memory;
+		String manufacture;
+		double getClockSpeed() {
+			return 5.5;}}}
+		
+
+
