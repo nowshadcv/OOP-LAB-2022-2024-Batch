ca976679f0872a8ebc3622b9e4c1952eda46649e
pavithrapavi358 <115776037+pavithrapavi358@users.noreply.github.com>
2023-04-03 15:18:29 +0530

    Add files via upload

---
+++ 40 Pavithra C R/07_Search/Linearsearch.java
@@ -0,0 +1,26 @@
+package packPavi;
+import java.util.Scanner;
+public class Linearsearch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int c,n,search,array[];
+		Scanner in =new Scanner(System.in);
+		System.out.println("enter number of elements");
+		n=in.nextInt();
+		array=new int[n];
+		System.out.println("enter those" +n+ "elements");
+		for(c=0;c<n;c++)
+			array[c]=in.nextInt();
+		System.out.println("enter th value you want to find:");
+		search=in.nextInt();
+		for(c=0;c<n;c++)
+			if(array[c]==search) {
+				System.out.println(search+" is present in the location:"+(c+1));
+				break;
+			}
+			if(c==n)
+				System.out.println(search+"is not present in the array.");
+	}
+
+}
