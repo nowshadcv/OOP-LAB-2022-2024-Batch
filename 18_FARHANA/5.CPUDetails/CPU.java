c7350e321867c9cf53017359a0875be0f124c54a
Farhananoushadpp <115775740+Farhananoushadpp@users.noreply.github.com>
2023-04-03 14:38:50 +0530

    Add files via upload

---
+++ 18_FARHANA/5.CPUDetails/CPU.java
@@ -0,0 +1,28 @@
+package packF;
+
+public class CPU {
+	double price;
+	class processor 
+	{
+		double cores;
+		String manufacturer;
+		double getCache()
+		{
+			return 4.3;
+		}
+	}
+	static class RAM
+	{
+		double memory;
+		String manufacturer;
+		double getClockSpeed()
+		{
+			return 5.5;
+		}
+	
+
+}
+
+
+
+}
