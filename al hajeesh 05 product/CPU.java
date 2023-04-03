e539408cabff84afe326c237526ecd1bd97b3535
Alhajeesh <115775546+Alhajeesh@users.noreply.github.com>
2023-04-03 15:30:38 +0530

    Add files via upload

---
+++ al hajeesh 05 product/CPU.java
@@ -0,0 +1,14 @@
+package Labcycle;
+
+public class CPU {
+	double price;
+	class processor {
+		double cores;
+		String manufacturer;
+		double getCache( ){
+			return 4.3 ;}}
+	static class RAM {
+		double memmory ;
+		String manufacturer;
+		double getClockSpeed() {
+			return 5.5 ;}}}
