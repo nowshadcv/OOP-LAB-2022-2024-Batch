4235c8f14b1c688b816c4b6cb7b38cdf8b11ad1b
mubashirakv <115776109+mubashirakv@users.noreply.github.com>
2023-04-03 14:33:31 +0530

    Add files via upload

---
+++ 36_Mubashira KV/labcycle/01_Product/Product.java
@@ -0,0 +1,31 @@
+package packA;
+  
+public class Product {
+	       String Pname,Pcode;
+	       int Price;
+	       public Product() {}
+	       public Product(String Pname,String Pcode,int Price) {
+	    	   this.Pname=Pname;
+	    	   this.Pcode=Pcode;
+	    	   this.Price=Price;}
+	       public void setPname(String Pname) {
+	    	   this.Pname=Pname;}
+	       public String getPcode() {
+	    	   return Pcode;}
+	       public String getPname() {
+	        	   return Pname;}
+	       public void setPcode(String Pcode) {
+	    	   this.Pcode=Pcode;}
+	       public int getPrice() {
+	    	   return Price;}
+	       public void setPrice(int Price) {
+	    	   this.Price=Price;}
+	       public void display() {
+	    	   System.out.println("Pcode:"+this.Pcode);
+	    	   System.out.println("Pname:"+this.Pname);
+	    	   System.out.println("Price:"+this.Price+"\n");
+	       }
+	}
+	    	   
+	      
+
