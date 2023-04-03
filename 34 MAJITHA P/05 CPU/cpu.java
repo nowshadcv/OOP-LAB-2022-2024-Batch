26a878a62fb8c744a1d8f5c909802c1763b5e0ae
Majitha-p <115775590+Majitha-p@users.noreply.github.com>
2023-04-03 15:31:15 +0530

    Add files via upload

---
+++ 34 MAJITHA P/05 CPU/cpu.java
@@ -0,0 +1,17 @@
+package packA;
+
+public class cpu {
+	double price;
+	class processor{
+		String manufacturer;
+		double getCache() {
+			return 4.3; }}
+	static class RAM{
+		double memory;
+		String manufacturer;
+		double getClockSpeed() {
+			return 5.5; 
+		}
+	}
+
+}
