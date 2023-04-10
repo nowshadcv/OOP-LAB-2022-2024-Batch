58130e600af6632ce07261affe1c0c6cc242ae9d
husnazakka <115775403+husnazakka@users.noreply.github.com>
2023-04-10 15:05:32 +0530

    Add files via upload

---
+++ 27 HUSNA KP/08.Employee/Employee.java
@@ -0,0 +1,16 @@
+package packHusna;
+
+public class Employee {
+	int eNo , eSalary ;
+	String eName ;
+	public Employee() {}
+	public Employee( int no, int sal , String name) {
+		eNo = no;
+		eSalary = sal;
+		eName = name;
+	}
+public void showData() {
+	System.out.println("EmpId = " + eNo + " " + "Name = " + eName + " " + "Salary = " + eSalary );
+	System.out.println();
+}
+}
