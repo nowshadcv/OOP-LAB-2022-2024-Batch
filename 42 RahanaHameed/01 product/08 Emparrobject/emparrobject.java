d3b49ec2994faae030e076667f752a23ddd0dfa9
Rahananinu <115775939+Rahananinu@users.noreply.github.com>
2023-04-10 05:28:34 -0400

    Add files via upload

---
+++ 42 RahanaHameed/01 product/08 Emparrobject/emparrobject.java
@@ -0,0 +1,47 @@
+package rahana;
+import java.util.Scanner;
+public class emparrobject {
+
+	public static void main(String[] args) {
+		System.out.println( "enter no.of employees:");
+		Scanner sc = new Scanner (System . in);
+		int n = sc.nextInt();
+		System.out.println("enter employee details one by one..");
+		Employee employees[ ] = new Employee [ n ];
+		Scanner scemp=new Scanner(System.in);
+				int eid , esal;
+		String ename;
+		for (int i=0;i<n;i++) {
+			System.out.println("enter "+i+"employee details ");
+			System.out.println("enter employee id (integer):");
+			eid=scemp.nextInt();
+			System.out.println("enter employee name (String):");
+			String name=scemp.next();
+			ename=new String (name);
+			System.out.println("enter employee salary(integer):");
+			esal=scemp.nextInt();
+			Employee emp = new Employee(eid , esal , ename);
+			employees [i]=emp;}
+		System.out.println("employees are:\n");
+		for(Employee y : employees)
+			y.showData();
+		System.out.println("enter employee number to search:");
+		int semp=sc.nextInt();
+		boolean found = false;
+		for (Employee e : employees) {
+			if (semp == e. eNo) {
+				found = true ;
+				System.out.println("employee found");
+				e. showData();
+				break;
+			}}
+		if (!found)
+			System.out.println("employee not found");
+		
+			}
+		
+		
+
+	}
+
+
