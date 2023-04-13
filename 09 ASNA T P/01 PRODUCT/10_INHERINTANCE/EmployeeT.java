6bcdd618fa603a0a696d37456917310cf6f99ee8
Asna587 <118503675+Asna587@users.noreply.github.com>
2023-04-13 15:54:10 +0530

    Add files via upload

---
+++ 09 ASNA T P/01 PRODUCT/10_INHERINTANCE/EmployeeT.java
@@ -0,0 +1,23 @@
+package PackA;
+import java.util.Scanner;
+class EmployeeT {
+	int empid;
+	String name;
+	float salary;
+	String address;
+	EmployeeT(){}
+	EmployeeT(int empid,String name,float salary,String address){
+		this.empid=empid;
+		this.name=name;
+		this.salary=salary;
+		this.address=address;
+		
+	}
+
+}
+
+
+
+
+
+
