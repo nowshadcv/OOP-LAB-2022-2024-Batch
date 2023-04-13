e5f0f65261423b92b0566abd52428d833caf1aaf
Sreelakshmi <68641711+sreeshi@users.noreply.github.com>
2023-04-13 15:35:47 +0530

    Add files via upload

---
+++ TeacherArrObjects.java
@@ -0,0 +1,69 @@
+package packSreelu;
+
+import java.util.Scanner;
+class EmployeeT{
+	int empid;
+	String name;
+	float salary;
+	String address;
+	EmployeeT(){}
+	EmployeeT(int empid,String name,float salary,String address){
+		this. empid=empid;
+		this.name=name;
+		this.salary=salary;
+		this.address=address;
+	}
+	
+}
+class Teacher extends EmployeeT{
+	String department,subject;
+	Teacher(int empid,String name,float salary,String address,String department,String subject){
+		super( empid, name,salary,address);
+		this.department=department;
+		this.subject=subject;}
+	public void display() {
+		System.out.println("teacher id:" +empid);
+		System.out.println("teacher name:" +name);
+		System.out.println("teacher salary:" +salary);
+		System.out.println("teacher address:" +address);
+		System.out.println("teacher department:" +department);
+		System.out.println("teacher subject:" +subject);	
+	}
+	
+}
+
+public class TeacherArrObjects {
+
+	public static void main(String[] args) {
+		System.out.println("enter no of teachers:");
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		System.out.println("enter teachers details one by one:");
+        Teacher teacher[]=new Teacher[n];
+        Scanner scT=new Scanner(System.in);
+        int tid;String name;float salary;String address;
+        String department;String subject;
+        for(int i=0;i<n;i++) {
+        	System.out.println("enter" +i+ "teachers details..");
+        	System.out.println("enter teacher id (integer)");
+        	tid=scT.nextInt();
+        	System.out.println("enter teacher name (string):");
+        	name=scT.next();
+        	System.out.println("enter teacher salary(float)");
+        	salary=scT.nextFloat();
+        	System.out.println("enter teacher address(string)");
+        	address=scT.next();
+        	System.out.println("enter teacher department(string)");
+        	department=scT.next();
+        	System.out.println("enter teacher subject(string)");
+        	subject=scT.next();
+            Teacher t=new Teacher (tid,name,salary,address,department,subject) ;
+            teacher[i]=t;}
+        System.out.println("teachers are:\n");
+        for(Teacher x:teacher) {
+        	x.display();System.out.println("\n");
+        
+        }
+        }
+
+}
