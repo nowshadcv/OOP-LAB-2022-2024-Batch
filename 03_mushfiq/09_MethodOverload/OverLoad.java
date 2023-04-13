380804a4322c85feda3f9f130c443f89ece3560d
ahmdmushfiq <115775322+ahmdmushfiq@users.noreply.github.com>
2023-04-13 14:54:20 +0530

    Add files via upload

---
+++ 03_mushfiq/09_MethodOverload/OverLoad.java
@@ -0,0 +1,14 @@
+package overload;
+
+public class OverLoad {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		OverLoadDemo ob = new OverLoadDemo();
+		ob.area(5);
+		ob.area(11,12);
+		ob.area(2.5);
+
+	}
+
+}
