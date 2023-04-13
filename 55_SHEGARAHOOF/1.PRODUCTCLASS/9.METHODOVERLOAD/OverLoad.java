d603edcb005718f8de4eaa7b3b03ce2f661453cb
shehaNaju <118175824+shehaNaju@users.noreply.github.com>
2023-04-13 15:31:43 +0530

    Add files via upload

---
+++ 55_SHEGARAHOOF/1.PRODUCTCLASS/9.METHODOVERLOAD/OverLoad.java
@@ -0,0 +1,17 @@
+package program9;
+
+public class OverLoad {
+
+	public static void main(String[] args) {
+		OverLoadDemo ob=new OverLoadDemo();
+		ob.area(5);
+		ob.area(11,12);
+		ob.area(2.5);
+
+
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
