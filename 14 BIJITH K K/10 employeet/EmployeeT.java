9865d489aaa8795c6215f55eeca4d5affe277315
bijithkk <115775535+bijithkk@users.noreply.github.com>
2023-04-13 15:35:40 +0530

    Add files via upload

---
+++ 14 BIJITH K K/10 employeet/EmployeeT.java
@@ -0,0 +1,14 @@
+package employeet;
+
+class EmployeeT {
+
+		int empid;
+		String name;
+		float salary;
+		String address;
+		EmployeeT(){}
+		EmployeeT(int empid,String name,float salary,String address){
+			this.empid = empid;
+			this.name = name;
+			this.salary = salary;
+			this.address = address; }}
