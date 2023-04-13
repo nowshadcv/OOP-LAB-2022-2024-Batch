c924fe9ae1dd72b74f38c0798550c589a6d35ca0
Farhananoushadpp <115775740+Farhananoushadpp@users.noreply.github.com>
2023-04-13 15:38:01 +0530

    Add files via upload

---
+++ 18_FARHANA/10.TeacherArrObjects/EmployeeT.java
@@ -0,0 +1,13 @@
+package packF;
+
+class EmployeeT { 
+        int empid; 
+        String name; 
+        float salary; 
+        String address; 
+        EmployeeT(){}
+        EmployeeT(int empid, String name, float salary, String address) { 
+               this.empid =empid; 
+               this.name = name; 
+               this.salary =salary; 
+               this.address = address; }} 
---
+++ 18_FARHANA/10.TeacherArrObjects/TeacherArrObjects.java
@@ -0,0 +1,42 @@
+package packF;
+
+import java.util.Scanner;
+
+public class TeacherArrObjects {
+
+	public static void main(String[] args) {
+		System.out.println("Enter Number of Teachers : ");
+		Scanner sc = new Scanner (System.in);
+		int n=sc.nextInt();
+		System.out.println("Enter Teacher Details One by One .. ");
+		Teacher teacher[]=new Teacher[n];
+		Scanner sct =new Scanner (System.in);
+		int tid; String name;float salary;String address;
+		String department ;
+		String subject;
+		for(int i=0;i<n;i++){
+			System.out.println("Enter " +i+ " Teacher details.. " );
+			System.out.println("Enter Teacher id (integer) : " );
+			tid=sct.nextInt();
+			System.out.println("Enter Teacher name (String) : " );
+			name=sct.next();
+			System.out.println("Enter Teacher salary (float) : " );
+			salary=sct.nextFloat();
+			System.out.println("Enter Teacher address (String) : " );
+			address=sct.next();
+			System.out.println("Enter Teacher Department (String) : " );
+			department=sct.next();
+			System.out.println("Enter Teacher Subject (String) : " );
+			subject=sct.next();
+			Teacher t = new Teacher (tid,name,salary,address,department,subject);
+			teacher[i]=t;
+		}
+		System.out.println("Teacher are : \n " );
+		for(Teacher x : teacher) {
+			x.display();
+			System.out.println("\n " );
+		}
+
+	}
+
+}
