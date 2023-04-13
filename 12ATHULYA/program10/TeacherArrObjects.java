fc005c109f3531c7c42a601766d803d1140dc069
athulya2022 <115775630+athulya2022@users.noreply.github.com>
2023-04-13 15:33:14 +0530

    Add files via upload

---
+++ 12ATHULYA/program10/TeacherArrObjects.java
@@ -0,0 +1,47 @@
+package program10;
+
+import java.util.Scanner;
+
+public class TeacherArrObjects {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(" Enter the number of Teachers : ");
+		Scanner sc = new Scanner(System.in);
+		int n =sc.nextInt();
+		System.out.println(" Enter Teacher details one by one ..");
+		Teacher teacher [] = new Teacher [n];
+		Scanner scT = new Scanner(System.in);
+		int tid;
+		String name;
+		float salary;
+		String address;
+		String department;
+		String subject;
+		for( int i=0;i<n;i++)
+		{
+			System.out.println(" Enter " + i + "teacher dtails ....");
+			System.out.println("Enter teacher id (integer) : ");
+			tid=scT.nextInt();
+			System.out.println("Enter teacher name (string) :");
+			name = scT.next();
+			System.out.println("Enter teacher salary (integer) : ");
+			salary = scT.nextFloat();
+			System.out.println("Enter teacher address (string) : ");
+			address =scT.next();
+			System.out.println("Enter teacher department (string) : ");
+			department =scT.next();
+			System.out.println("Enter teacher subject (string) : ");
+			subject =scT.next();	
+			Teacher t = new Teacher ( tid , name, salary, address , department, subject);
+			teacher [i] =t;
+			}
+		System.out.println("Teachers are : \n ");
+		for(Teacher x : teacher){
+			x.display();
+			System.out.println("\n");
+		}
+
+	}
+
+}
