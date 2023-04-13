126335bf84fd0a931e660b624c71550484a747d4
hibarasheed17 <69135452+hibarasheed17@users.noreply.github.com>
2023-04-13 15:02:56 +0530

    Add files via upload

---
+++ 26_HIBA/09 Overloading/OverLoad.java
@@ -0,0 +1,24 @@
+package packB;
+
+class OverLoadDemo{
+	void area(float x) {
+		System.out.println("The area of the sqare is "+ Math.pow(x, 2)+ " sq units");
+	}
+	void area(float x, float y) {
+		System.out.println("The area of the rectangle is "+ x*y + " sq units");
+	}
+	void area(double x) {
+		double z = 3.14 * x * x;
+		System.out.println("The area of the circle is "+ z + " sq units");
+	}
+}
+
+class OverLoad {
+	public static void main(String args[]) {
+		OverLoadDemo ob = new OverLoadDemo();
+		ob.area(5);
+		ob.area(11,12);
+		ob.area(2.5);
+	}
+
+}
