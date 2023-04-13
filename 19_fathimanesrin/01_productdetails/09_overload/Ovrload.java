7322bfe430cbdb6b15149c5038885a94ebb675bb
fathimanesri <118171265+fathimanesri@users.noreply.github.com>
2023-04-13 15:26:12 +0530

    Add files via upload

---
+++ 19_fathimanesrin/01_productdetails/09_overload/Ovrload.java
@@ -0,0 +1,14 @@
+package packA;
+
+ class Ovrload {
+
+	public static void main(String[] args) {
+		overloaddemo ob=new overloaddemo();
+		 ob.area(5);
+		 ob.area(11,12);
+		 ob.area(2,5);
+		
+
+	}
+
+}
---
+++ 19_fathimanesrin/01_productdetails/09_overload/overloaddemo.java
@@ -0,0 +1,13 @@
+package packA;
+
+ class overloaddemo {
+	 void area(float x) {
+		  System.out.println("the arae of the square is"+Math.pow(x, 2)+"sq unit");}
+	 void area(float x,float y) {
+		 System.out.println("the arae of the rectangle is" +x*y+" sq unit");}
+	 void area(double x) {
+		 double z =3.14 * x * x;
+		 System.out.println("the arae of the circle is"+z+"sq unit");}}
+ 
+	 
+
