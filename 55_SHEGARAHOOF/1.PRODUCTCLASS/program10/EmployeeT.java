8213d81f8c9d3311dcbed27614ea167e3bd9ccd5
shehaNaju <118175824+shehaNaju@users.noreply.github.com>
2023-04-13 15:41:16 +0530

    Add files via upload

---
+++ 55_SHEGARAHOOF/1.PRODUCTCLASS/program10/EmployeeT.java
@@ -0,0 +1,20 @@
+package program10;
+
+public class EmployeeT {
+	int empid;
+	String name;
+	float salary;
+	String address;
+	EmployeeT(){}
+	EmployeeT(int empid, String name,float salary,String address){
+		this.empid=empid;
+		this.name=name;
+		this.salary=salary;
+		this.address=address;
+
+
+
+		
+	}
+
+}
