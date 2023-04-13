410205c567c599d49483104da81b0d7a869cae0d
fidhaabid <115775652+fidhaabid@users.noreply.github.com>
2023-04-13 15:00:05 +0530

    Add files via upload

---
+++ 22_fidhaabid/9.OVERLOAD/OverLoad.java
@@ -0,0 +1,14 @@
+package co2.java;
+
+public class OverLoad {
+	public static void main(String args[]){
+		OverLoadDemo ob = new OverLoadDemo();
+				ob.area(5);
+				ob.area(11,12);
+				ob.area(2.5);
+		
+	}
+			
+
+
+}
