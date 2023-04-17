00f97139844bc3eb6490f3f72236cf4e643841c8
hibarasheed17 <69135452+hibarasheed17@users.noreply.github.com>
2023-04-17 15:17:58 +0530

    Add files via upload

---
+++ 26_HIBA/11. Interface - Circle and Rectangle/AP.java
@@ -0,0 +1,9 @@
+package packB;
+
+import java.util.Scanner;
+
+interface AP {
+	void input();
+	void area();
+	void perimeter();
+}
---
+++ 26_HIBA/11. Interface - Circle and Rectangle/Square.java
@@ -0,0 +1,20 @@
+package packB;
+
+import java.util.Scanner;
+
+class Square implements AP {
+	int a = 0;
+	double area, perimeter;
+	public void input() {
+		Scanner s = new Scanner(System.in);
+		a = s.nextInt();
+	}
+	public void area() {
+		area = a * a;
+		System.out.println("Area of square: " + area);
+	}
+	public void perimeter() {
+		perimeter = 4 * a;
+		System.out.println("Perimeter of square: " + perimeter);
+	}
+}
