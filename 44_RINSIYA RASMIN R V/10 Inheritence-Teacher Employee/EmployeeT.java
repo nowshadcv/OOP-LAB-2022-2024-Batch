7bd739fb50a4c0d702996f959a3ab902f29be402
rinsiyarasmin <96901578+rinsiyarasmin@users.noreply.github.com>
2023-04-24 16:36:06 +0530

    Add files via upload

---
+++ 44_RINSIYA RASMIN R V/10 Inheritence-Teacher Employee/EmployeeT.java
@@ -0,0 +1,28 @@
+package rinsiyarasmin;
+
+import java.util.Scanner;
+class EmployeeT {
+        int empid;
+        String name;
+        float salary;
+        String address;
+        EmployeeT(int empid,String name,float salary,String address){
+        	    this.empid = empid;
+        	    this.name = name;
+        	    this.salary = salary;
+        	    this.address = address; }}
+class Teacher extends EmployeeT{
+	   String department, subject;
+	   Teacher(int empid, String name, float salary, String address, String department, String subject){
+		       super(empid,name,salary,address);
+		       this.department =  department;
+		       this.subject = subject;}
+	   public void display() {
+		   System.out.println("teacher id : " + empid);
+		   System.out.println("teacher name : " + name);
+		   System.out.println("teacher salary : " + salary);
+		   System.out.println("teacher address : " + address);
+		   System.out.println("teacher department : " + department);
+		   System.out.println("teacher subject : " + subject);
+        }
+}
