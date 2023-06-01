0f097b1a4889bc951b00400ce61f482b2add6e64
ahmdmushfiq <115775322+ahmdmushfiq@users.noreply.github.com>
2023-06-01 16:19:36 +0530

    Add files via upload

---
+++ 03_mushfiq/12multilevel_inheritance/Person.java
@@ -0,0 +1,18 @@
+package multilevel_inheritance;
+
+public class Person {
+
+	 String name;
+     String gender;
+     String address;
+     int age;
+     Person(){}
+     Person(String name, String gender, String address,int age){
+         this.name = name;
+         this.gender = gender;
+         this.address = address;
+         this.age = age;}}
+
+
+
+
