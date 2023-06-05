1f82e2d15d095bdf5092578e5d66577d69623a58
pavithrapavi358 <115776037+pavithrapavi358@users.noreply.github.com>
2023-06-05 15:42:28 +0530

    Add files via upload

---
+++ 40 Pavithra C R/14_InheritanceBook/InheritanceBook.java
@@ -0,0 +1,54 @@
+package packPavi;
+class Publisher{
+	String Publisher;
+	Publisher(String publi){
+		this.Publisher=publi;
+	}
+}
+class Book{
+	String name;
+	Publisher publisher;
+	Book(){}
+		public Book(String name,Publisher publisher) {
+			this.name=name;
+			this.publisher=publisher;
+			
+		}
+	}
+	class Literature extends Book{
+		String Lit_type="Literature";
+		Literature (String name,Publisher publisher){
+			super(name,publisher);
+		}
+		void display() {
+			System.out.println("Name:"+super.name);
+			System.out.println("Type:"+this.Lit_type);
+			System.out.println("publisher:"+this.publisher.Publisher);
+			
+		}
+	}
+class Fiction extends Book{
+	String Lit_type="Fiction";
+	Fiction(String name,Publisher publisher){
+		super(name,publisher);
+	}
+	void display() {
+		System.out.println("Name:"+super.name);
+		System.out.println("Type:"+this.Lit_type);
+		System.out.println("publisher:"+this.publisher.Publisher);
+}
+}
+public class InheritanceBook {
+
+	public static void main(String[] args) {
+		Publisher lp= new Publisher("s . Chand");
+		Literature l= new Literature("As you like it",lp);
+		l.display();
+		Publisher fp = new Publisher("Take MCGraw Hill");
+		Fiction f = new Fiction("Tempest",fp);
+		f.display();
+		// TODO Auto-generated method stub
+
+	}
+
+}
