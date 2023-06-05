b0af44a1469cef076e8ea2e5ac3446463bd92d8d
shehaNaju <118175824+shehaNaju@users.noreply.github.com>
2023-06-05 15:14:19 +0530

    Add files via upload

--- 10_ATHILA N/11.INTERFACE/Square.java
+++ 10_ATHILA N/11.INTERFACE/Square.java
@@ -1,20 +1,33 @@
-package pgm11;
+package program11;
 
 import java.util.Scanner;
 
-class Square implements AP{
-	int l=0;
-	double area,perimeter;
-	public void input() {
-		Scanner r = new Scanner(System.in);
-		l=r.nextInt();
-	}
-	public void area(){
-		area = l*l;
-		System.out.println("area of square: " +area);
-	}
-	public void perimeter(){
-		perimeter = 4*l;
-		System.out.println("Perimeter of square: " +perimeter);
-	}
-}
+
+  class Sqaure implements AP {
+     int a=0;
+     double area,perimeter;
+	 public void input(){
+		 Scanner r=new Scanner(System.in);
+		 a=s.nextInt();
+		
+		 }
+	 public void area(){
+		 area=Math.pow(a, 2);
+		 System.out.println("Area of square:"+area);
+	 }
+	 public void perimeter(){
+		 perimeter=4*a;
+		 System.out.println("perimeter of square:"+perimeter);
+	 }
+  
+	 	 
+    	   
+       }
+
+
+
+
+
+	
+
+
