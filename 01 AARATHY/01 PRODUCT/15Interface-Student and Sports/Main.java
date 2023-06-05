ad2b9b1a40bd3a50a92fdb9ea0dea1ebf3f3afd2
Arathy07 <115775427+Arathy07@users.noreply.github.com>
2023-06-05 16:15:53 +0530

    Add files via upload

---
+++ 01 AARATHY/01 PRODUCT/15Interface-Student and Sports/Main.java
@@ -0,0 +1,74 @@
+package aarathyps;
+
+
+import java.io.*;
+import java.util.Scanner;
+
+interface Sports{
+	public void sports_getData();
+	public void sports_dispData();
+	
+}
+class Student{
+	String name;
+	int roll_no;
+	float mark1,mark2;
+	Student(String n,int r,float m1,float m2){
+		name=n;
+		roll_no=r;
+		mark1=m1;
+		mark2=m2;
+	}
+void display() {
+	System.out.println("Student Details");
+	System.out.println(" Name of Student: "+name);
+	System.out.println("Roll No of student:"+roll_no);
+	System.out.println("Marks of subject1:"+mark1);
+	System.out.println("Marks of subject2"+mark2);
+}
+}
+class Result extends Student implements Sports{
+	int r;
+	String item;
+	Scanner sc=new Scanner(System.in);
+	Result(String n,int r,float m1,float m2){
+		super(n,r,m1,m2);
+	}
+public void academic() {
+	float total=(mark1+mark2);
+	System.out.println("___Academic info____");
+}
+public void sports_getData() {
+	System.out.print("Enter the sports item which student participated:");
+	item=sc.nextLine();
+	System.out.print("Enter the rank postion that the obtained:");
+	r=sc.nextInt();
+	
+}
+public void sports_dispData() {
+	System.out.println("_____Sports info_____");
+	System.out.println("item:"+item);
+	System.out.println("Rank:"+r);
+}
+}
+public class Main {
+	public static void main(String args[] )throws IOException {
+		InputStreamReader isr= new InputStreamReader(System.in);
+		BufferedReader br=new BufferedReader(isr);
+		System.out.print("Enter the name:");
+		String n =br.readLine();
+		System.out.print("Enter roll no:");
+		int roll= Integer.parseInt(br.readLine());
+		System.out.print("Enter the mark in first subject:");
+		float m1=Float.parseFloat(br.readLine());
+		System.out.print("Enter the mark in second subject:");
+		float m2=Float.parseFloat(br.readLine());
+		Result re=new Result(n,roll,m1,m2);
+		re.sports_getData();
+		re.display();
+		re.academic();
+		re.sports_dispData();
+		
+	}
+}
+
