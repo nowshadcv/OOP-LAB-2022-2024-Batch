761534dc5e98a85586d2ba6d011fcef2f24a9f93
FIDHARAZAKPA <115776269+FIDHARAZAKPA@users.noreply.github.com>
2023-06-12 15:16:46 +0530

    Add files via upload

---
+++ 23 fidharazak/Inheritance/EmployeeT.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class EmployeeT {
+	int empid;
+	String name;
+	float salary;
+	String address;
+	EmployeeT(){}
+	EmployeeT(int empid,String name,float salary,String address){
+		this.empid=empid;
+		this.name=name;
+		this.salary=salary;
+		this.address=address;
+		
+	}
+	
+
+}
