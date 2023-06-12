795783abdd9ad0fe0f18ff99fa9e8b14fb0dcf8b
Rahananinu <115775939+Rahananinu@users.noreply.github.com>
2023-06-12 06:02:38 -0400

    Add files via upload

---
+++ 42 RahanaHameed/01 product/16.package-User defined graphics packages/UserDefinedPackage/src/figures/Rectanglee.java
@@ -0,0 +1,14 @@
+package figures;
+
+public class Rectanglee implements FigureRe{
+	private int l,b;
+	public Rectanglee(int lenght, int breadth) {
+		lenght=l; breadth =b;}
+	public int perimeter() {return (2*l+b);}
+	public int area() {
+		return (l*b);
+	}
+		
+
+
+}
