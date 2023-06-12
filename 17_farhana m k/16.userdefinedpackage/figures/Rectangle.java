0c2f28de19560086f84476e0f6c8c71655dcbe87
FARHANA M K <115775974+farhana003@users.noreply.github.com>
2023-06-12 15:41:36 +0530

    Add files via upload

---
+++ 17_farhana m k/16.userdefinedpackage/figures/Rectangle.java
@@ -0,0 +1,9 @@
+package figures;
+
+public class Rectangle implements FigureRt{
+	private int length;int breadth;
+	public Rectangle(int l,int b) {length=l;breadth=b;}
+	public int perimeter() {return 2*(length+breadth);} 
+	public int area() {return(length*breadth);}
+
+}
