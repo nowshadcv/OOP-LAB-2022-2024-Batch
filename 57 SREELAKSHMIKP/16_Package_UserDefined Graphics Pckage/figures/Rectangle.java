aefac462819404587da59bacf4458ddd1047edf4
Sreelakshmi <68641711+sreeshi@users.noreply.github.com>
2023-06-12 15:37:54 +0530

    Add files via upload

---
+++ 57 SREELAKSHMIKP/16_Package_UserDefined Graphics Pckage/figures/Rectangle.java
@@ -0,0 +1,18 @@
+package figures;
+
+public class Rectangle implements FigureRe {
+	private double length,breadth;
+	public Rectangle(double l,double b) { 
+		length=l; breadth=b;}
+	public double perimeter () {
+		return 2*(length+breadth);
+		}
+	public double area() { 
+		
+		return (length*breadth);
+		}}
+
+
+
+
+
