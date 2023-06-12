a0aef362506d20465739adf271b529169ae69270
athulya2022 <115775630+athulya2022@users.noreply.github.com>
2023-06-12 15:36:28 +0530

    Add files via upload

---
+++ 12ATHULYA/program11/Circle.java
@@ -0,0 +1,23 @@
+package program11;
+
+import java.util.Scanner;
+
+
+interface AP{
+	void input();
+	void area();
+	void perimeter();}
+
+class Circle implements AP{
+	int r=0;
+	double pi = 3.14,area=0,perimeter=0;
+	public void input(){
+		Scanner c=new Scanner(System.in);
+		r=c.nextInt();}
+	public void area(){
+		area=pi*r*r;
+		System.out.println("Area of circle:"+ area);}
+	public void perimeter(){
+		perimeter=2*pi*r;
+		System.out.println("Perimeter of circle:" + perimeter);}}
+		
