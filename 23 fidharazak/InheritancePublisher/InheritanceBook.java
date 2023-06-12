ebad82ff6efd521b334fbfa9560c5e50ed24c703
FIDHARAZAKPA <115776269+FIDHARAZAKPA@users.noreply.github.com>
2023-06-12 16:06:42 +0530

    Add files via upload

---
+++ 23 fidharazak/InheritancePublisher/InheritanceBook.java
@@ -0,0 +1,18 @@
+
+public class InheritanceBook {
+
+	public static void main(String[] args) {
+		Publisher lp=new Publisher ("S . Chand") ;
+		Literature l = new Literature ("As you like it" , lp);
+		l . display();
+		Publisher fp=new Publisher("Tata McGraw Hill");
+		Fiction f = new Fiction ("Tempest " , fp) ;
+		f . display ( );}}
+		
+
+	
+		// TODO Auto-generated method stub
+
+	
+
+
