485b9620e0a3b156838a12805a488343fc99eb39
binshaunnikrishnan <115775988+binshaunnikrishnan@users.noreply.github.com>
2023-06-12 16:27:23 +0530

    Add files via upload

---
+++ 15 BINSHAUNNIKRISHNAN/18_Package-Arithmetic Package/Arithmetic/Subtraction.java
@@ -0,0 +1,9 @@
+package Arithmetic;
+
+public class Subtraction implements ArithmeticSub {
+	private int number1,number2;
+	public Subtraction(int n1,int n2) {number1 = n1; number2 = n2;}
+	public int subtraction() {return(number1+number2);}
+
+
+}
---
+++ 15 BINSHAUNNIKRISHNAN/18_Package-Arithmetic Package/module-info.java
@@ -0,0 +1,2 @@
+module UserDefinedPackage {
+}
