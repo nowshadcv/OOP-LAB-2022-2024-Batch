1ccb26c3b16a46a184d9b5a932b1f26c49e8618d
fidhaabid <115775652+fidhaabid@users.noreply.github.com>
2023-06-19 16:29:05 +0530

    Add files via upload

---
+++ 22_fidhaabid/18.Package-Arithmetic Package/Arithmetic/Division.java
@@ -0,0 +1,10 @@
+package Arithmetic;
+
+public class Division implements ArithDiv{
+	private double num1,num2;
+	public Division(double a,double b) {
+		num1=a; num2=b;}
+	public double div() {return(num1/num2);}
+	} 
+
+
