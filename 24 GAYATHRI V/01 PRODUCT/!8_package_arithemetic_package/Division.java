e11fdb876b5e80c8fc565d37e95f6820d5b963da
gayathriamm <115775764+gayathriamm@users.noreply.github.com>
2023-06-19 16:01:59 +0530

    Delete Division.java

--- 24 GAYATHRI V/01 PRODUCT/!8_package_arithemetic_package/Division.java
+++
@@ -1,9 +0,0 @@
-package Arithematic;
-
-public class Division implements AriDiv {
-	private double num1,num2;
-	public Division(double n1,double n2) {num1=n1;num2=n2;}
-		public double div() {return num1/num2;}
-		
-
-}
