31bf9928d35352891d9bf25930e4bc704e07b7fc
gayathriamm <115775764+gayathriamm@users.noreply.github.com>
2023-06-19 16:02:22 +0530

    Delete Substraction.java

--- 24 GAYATHRI V/01 PRODUCT/!8_package_arithemetic_package/Substraction.java
+++
@@ -1,9 +0,0 @@
-package Arithematic;
-
-public class Substraction implements AriSub {
-	private double num1,num2;
-	public Substraction(double n1,double n2) {num1=n1;num2=n2;}
-		public double Sub() {return num1-num2;}
-		
-
-}
