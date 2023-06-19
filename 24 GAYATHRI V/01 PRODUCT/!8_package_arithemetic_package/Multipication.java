010fb3d211fe5f4d5d03db20a3dcfe9d52d045da
gayathriamm <115775764+gayathriamm@users.noreply.github.com>
2023-06-19 16:02:10 +0530

    Delete Multipication.java

--- 24 GAYATHRI V/01 PRODUCT/!8_package_arithemetic_package/Multipication.java
+++
@@ -1,9 +0,0 @@
-package Arithematic;
-
-public class Multipication implements AriMul {
-	private double num1,num2;
-	public Multipication(double n1,double n2) {num1=n1;num2=n2;}
-		public double mul() {return num1*num2;}
-		
-
-}
