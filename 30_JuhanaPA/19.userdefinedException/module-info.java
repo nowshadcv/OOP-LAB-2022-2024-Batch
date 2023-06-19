c6713334eef757b4b2d30ddcc0f04b22cb176752
juhanapa09 <115775860+juhanapa09@users.noreply.github.com>
2023-06-19 15:48:27 +0530

    Add files via upload

---
+++ 30_JuhanaPA/19.userdefinedException/module-info.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+class UsernameException extends Exception {
+	public UsernameException(String msg) { super(msg); }}
+
+class PasswordException extends Exception {
+	public PasswordException(String msg)  { super(msg); }}
+
