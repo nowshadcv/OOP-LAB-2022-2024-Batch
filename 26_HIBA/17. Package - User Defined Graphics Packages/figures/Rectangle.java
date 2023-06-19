7bc3aba8da79b51788c001d269ba61f31ba46135
hibarasheed17 <69135452+hibarasheed17@users.noreply.github.com>
2023-06-19 16:34:15 +0530

    Add files via upload

---
+++ 26_HIBA/17. Package - User Defined Graphics Packages/figures/Rectangle.java
@@ -0,0 +1,17 @@
+package figures;
+
+public class Rectangle implements FigureRec {
+	private double length;
+	private double width;
+	public Rectangle(double l,double w) {
+		length=l;
+		width=w;
+	}
+	public double perimeter() {
+		return (2*(length+width));
+	}
+	public double area() {
+		return (length*width);
+	}
+	
+}
