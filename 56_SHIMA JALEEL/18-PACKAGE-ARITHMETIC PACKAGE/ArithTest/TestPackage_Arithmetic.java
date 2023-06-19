89078cfe0f71fbbb97d3c52349df3e1af9d87a29
Shima-Jaleel <123296021+Shima-Jaleel@users.noreply.github.com>
2023-06-19 16:45:59 +0530

    Add files via upload

---
+++ 56_SHIMA JALEEL/18-PACKAGE-ARITHMETIC PACKAGE/ArithTest/TestPackage_Arithmetic.java
@@ -0,0 +1,40 @@
+package ArithTest;
+
+import java.util.Scanner;
+
+import Operation.*;
+
+
+public class TestPackage_Arithmetic {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.println("Enter the first number:");
+		int a = sc.nextInt();
+		System.out.println("Enter the second number:");
+		int b= sc.nextInt();
+		Addition add=new Addition(a,b);
+		System.out.println("Addition"+add.add());
+		System.out.println("Enter the first number:");
+		int c = sc.nextInt();
+		System.out.println("Enter the second number:");
+		int d= sc.nextInt();
+		Subtraction sub=new Subtraction(c,d);
+		System.out.println("Subtraction"+sub.sub());
+		System.out.println("Enter the first number:");
+		int m = sc.nextInt();
+		System.out.println("Enter the second number:");
+		int n= sc.nextInt();
+		Multiplication mul=new Multiplication(m,n);
+		System.out.println("Multiplication"+mul.mul());
+		System.out.println("Enter the first number:");
+		int x = sc.nextInt();
+		System.out.println("Enter the second number:");
+		int y= sc.nextInt();
+		Division div=new Division(x,y);
+		System.out.println("Addition"+div.div());
+
+	}
+
+}
