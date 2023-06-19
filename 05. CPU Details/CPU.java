6a02e7ef586752f42fdfc7988b9cae57c490ef7a
shahanasajil123 <117262410+shahanasajil123@users.noreply.github.com>
2023-06-19 16:25:13 +0530

    Add files via upload

---
+++ 05. CPU Details/CPU.java
@@ -0,0 +1,24 @@
+package packA;
+
+public class CPU {
+	double Price;
+	class processor
+	{
+		double cores;
+		String manufacturer;
+		double getCach()
+		{
+			return 4.3;
+		}
+	}
+	static class RAM
+	{
+		double memory;
+		String manufacturer;
+		double getClockSpeed()
+		{
+			return 5.5;
+		}
+	}
+
+}
---
+++ 52 Shahana P K/01. Product Class/Product.java
@@ -0,0 +1,45 @@
+package packA;
+
+public class Product {
+	
+		String pname,pcode;
+		int price;
+		public Product() {}
+		public Product (String pname, String pcode, int price)
+		{
+			this.pname=pname;
+			this.pcode=pcode;
+			this.price=price;
+			}
+		public void Setname(String pname)
+		{
+			this.pname=pname;
+		}
+		public String getpcode()
+		{
+			return pcode;
+		}
+		public String getpname()
+		{
+			return pname;
+		}
+		public void setpcode(String pcode)
+		{
+			this.pcode=pcode;
+		}
+		public int getPrice() {
+			return price;
+		
+		}
+		public void setprice(int price)
+		{
+			this.price=price;
+		}
+		public void display()
+		{
+			System.out.println("pcode : "+this.pcode);
+			System.out.println("pname : "+this.pname);
+			System.out.println("price : "+this.price+"\n");
+		}
+		}
+
