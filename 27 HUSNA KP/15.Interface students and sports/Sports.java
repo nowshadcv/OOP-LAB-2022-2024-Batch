111d2926d98b98fb2248599da2e464433ae6cf86
husnazakka <115775403+husnazakka@users.noreply.github.com>
2023-06-19 16:38:35 +0530

    Add files via upload

---
+++ 27 HUSNA KP/15.Interface students and sports/Sports.java
@@ -0,0 +1,6 @@
+package packHusna;
+
+public interface Sports {
+	int score =25;
+	void displaySportsScore();
+}
---
+++ 27 HUSNA KP/15.Interface students and sports/Student.java
@@ -0,0 +1,6 @@
+package packHusna;
+
+public interface Student {
+	int score=10;
+	void displayScore();
+}
