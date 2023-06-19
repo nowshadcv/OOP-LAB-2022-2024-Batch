88424d4f930efe6b96b39bd0f02ede973e672622
saranyadask <115775733+saranyadask@users.noreply.github.com>
2023-06-19 16:00:48 +0530

    Add files via upload

---
+++ 51 Saranya Das K/18.Package-Arithametic Package/FULL/Arithamatic/src/ArithamaticPackage/Mul.java
@@ -0,0 +1,16 @@
+package ArithamaticPackage;
+
+public class Mul implements MultiplicationInterface 
+{
+	int num1,num2;
+	public Mul(int a ,int b)
+	{
+		num1=a;
+		num2=b;
+	}
+	public int mul()
+	{
+		return num1*num2;
+	}
+
+}
