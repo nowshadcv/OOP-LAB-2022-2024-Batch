98b8331475f9f75cb450daae741b463cd3e93cb3
ahmdmushfiq <115775322+ahmdmushfiq@users.noreply.github.com>
2023-06-19 14:51:56 +0530

    Add files via upload

---
+++ 03_mushfiq/18.Arithmtc/Arithematic/Addition.java
@@ -0,0 +1,7 @@
+package Arithematic;
+
+public class Addition implements ArithmtcAdd{
+private int num1,num2;
+public  Addition(int n1,int n2) {num1=n1;num2=n2;}
+public int sum() {return num1+num2;}
+}
---
+++ 03_mushfiq/18.Arithmtc/Arithematic/Multiplication.java
@@ -0,0 +1,8 @@
+package Arithematic;
+
+public class Multiplication implements ArithmtcMul{
+	private int num1,num2;
+	public  Multiplication(int n1,int n2) {num1=n1;num2=n2;}
+	public int mul() {return num1*num2;}
+	
+}
