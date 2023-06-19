0f07c4d0a426dc652de1c69416d047a212809c19
FIDHARAZAKPA <115776269+FIDHARAZAKPA@users.noreply.github.com>
2023-06-19 15:40:08 +0530

    Add files via upload

---
+++ 23 fidharazak/UserDefinedException/PasswordException.java
@@ -0,0 +1,5 @@
+
+ class PasswordException extends Exception{
+	 public PasswordException(String msg){super(msg);}
+
+}
---
+++ 23 fidharazak/UserDefinedException/UsernameException.java
@@ -0,0 +1,5 @@
+import java.util.Scanner;
+class UsernameException extends Exception{
+	public UsernameException(String msg){super(msg);}
+
+}
