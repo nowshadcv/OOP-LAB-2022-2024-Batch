a5f01f8cdf9b81b5d277197a18d84de19b35ebc9
jumnasidheeq <115775528+jumnasidheeq@users.noreply.github.com>
2023-06-21 10:21:55 +0530

    Add files via upload

---
+++ 32 JUMNA/13.Interface-Customer order/BillGen.java
@@ -0,0 +1,17 @@
+package packAco2;
+public interface BillGen {
+	int calculate();
+}
+class ProductB implements BillGen{ 
+	String name; 
+	int prod_id, quantity, unit_price, total;
+	 ProductB(){}
+	 ProductB(String n, int p, int q, int u){
+		 name = n; 
+		 prod_id = p; 
+		 quantity = q;
+		 unit_price =u;
+	 }
+	 public int calculate(){
+		 total = quantity* unit_price;
+		 return total;}}
