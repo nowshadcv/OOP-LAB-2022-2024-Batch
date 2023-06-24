8b4374dc64c072b4ba3d3df3fa08a6822875923e
FIDHARAZAKPA <115776269+FIDHARAZAKPA@users.noreply.github.com>
2023-06-24 16:10:06 +0530

    Add files via upload

---
+++ 23_FIDHA RAZAK PA/22.Multiplication/MultiplicationPrimeThreads.java
@@ -0,0 +1,13 @@
+
+public class MultiplicationPrimeThreads {
+
+	public static void main(String[] args) {
+		MultiplicationT ob1=new MultiplicationT();
+		PrimeN ob2= new PrimeN();
+		ob1.t1.start();
+		ob2.t2.start();
+		// TODO Auto-generated method stub
+
+	}
+
+}
