a74c2b1712f1c4ef06ddb43c224a7bc42c45d765
muhazeena_fathima <115775914+muhzieeh@users.noreply.github.com>
2023-06-24 16:39:31 +0530

    Add files via upload

---
+++ 38_MUHAZEENA FATHIMA/18. Package - Arithmetic Package/[Used Pakages]/ArithmeticPackage/Sub.java
@@ -0,0 +1,12 @@
+package ArithmeticPackage;
+
+public class Sub implements SubstractionInterface {
+	int num1,num2;
+	public Sub (int a,int b) {
+		num1=a;
+		num2=b;
+	}
+	public int sub() {
+		return num1-num2;
+	}
+}
