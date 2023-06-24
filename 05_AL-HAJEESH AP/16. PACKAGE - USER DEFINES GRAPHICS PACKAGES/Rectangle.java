ebe1c2ea571211b5a672c591614dd16fbaa4e8ba
Alhajeesh <115775546+Alhajeesh@users.noreply.github.com>
2023-06-24 16:34:25 +0530

    Add files via upload

---
+++ 05_AL-HAJEESH AP/16. PACKAGE - USER DEFINES GRAPHICS PACKAGES/Rectangle.java
@@ -0,0 +1,11 @@
+package figures;
+
+public class Rectangle implements FigureRe{
+	private int length,breadth;
+	public Rectangle(int l,b) {;}
+	public int perimeter() {return (4*side);}
+	public int area() {return (side*side);}
+	
+	} {
+
+}
