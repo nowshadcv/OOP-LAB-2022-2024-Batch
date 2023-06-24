9d0a136605af8613968ab60f63ffe07d9ce93348
jumanapa07 <115775971+jumanapa07@users.noreply.github.com>
2023-06-24 16:22:08 +0530

    Add files via upload

---
+++ 31_Jumana_P_A/12.Interface/Square.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class Square implements AP{
+	
+	
+		int l=0;
+		double area,perimeter;
+		public void input() {
+			Scanner r= new Scanner(System.in);
+			l=r.nextInt();
+			
+			
+		}
+		public void area() {
+			area=l*l;
+			System.out.println("Area of square: "+area);
+		}
+		public void perimeter() {
+			perimeter=4*l;
+			System.out.println("Perimeter of square: "+perimeter);
+		}
+	}
+
+
