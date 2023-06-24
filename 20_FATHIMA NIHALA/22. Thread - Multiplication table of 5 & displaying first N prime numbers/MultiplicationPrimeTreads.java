738238893b2ac334e72e39a216f74bba475f4b9f
Fathimanihala <99164434+Fathimanihala@users.noreply.github.com>
2023-06-24 16:39:11 +0530

    Add files via upload

---
+++ 20_FATHIMA NIHALA/22. Thread - Multiplication table of 5 & displaying first N prime numbers/MultiplicationPrimeTreads.java
@@ -0,0 +1,16 @@
+package PackA;
+
+public class MultiplicationPrimeTreads {
+
+	public static void main(String[] args) {
+	
+		
+		MultiplicationT ob1 = new MultiplicationT();
+		PrimeN ob2 = new PrimeN();
+		ob1.t1.start();
+		ob2.t2.start();
+		 }
+
+		
+
+}
