dce1e8637b33598d6fb4989a20c5c2ec88666486
Alhajeesh <115775546+Alhajeesh@users.noreply.github.com>
2023-06-24 16:58:22 +0530

    Add files via upload

---
+++ 05_AL-HAJEESH AP/10.INHERITANCE/program10/EmployeeT.java
@@ -0,0 +1,13 @@
+package program10.java;
+import java.util.Scanner;
+class EmployeeT {
+	int empid;
+	String name;
+	float salary;
+	String address;
+	EmployeeT(){}
+	EmployeeT(int empid, String name, float salary, String address){
+		this.empid = empid;
+		this.name = name;
+		this.salary = salary;
+		this.address = address; }}
