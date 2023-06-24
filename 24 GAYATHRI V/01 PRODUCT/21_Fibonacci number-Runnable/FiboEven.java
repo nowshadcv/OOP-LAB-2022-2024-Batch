44cf63132844a67796507030bd8950b8e0a05434
gayathriamm <115775764+gayathriamm@users.noreply.github.com>
2023-06-24 16:26:33 +0530

    Add files via upload

---
+++ 24 GAYATHRI V/01 PRODUCT/21_Fibonacci number-Runnable/FiboEven.java
@@ -0,0 +1,52 @@
+package Test;
+class Fibonacci implements Runnable{
+	String name;
+	Thread t;
+	Fibonacci (String thread){
+		name=thread;
+		t=new Thread(this,name);
+		System.out.println("Fibnocci:"+t);
+		t.start();}
+		public void run() {
+			int n1=0,n2=1,n3,i,count=10;
+			System.out.print(n1+" "+n2);
+			for(i=2;i<count;++i) {
+				n3=n1+n2;
+				System.out.print(" "+n3);
+				n1=n2;n2=n3;
+		}
+	}
+}
+class Even implements Runnable{
+	String name;
+	Thread t;
+	Even(String thread){
+		name=thread;
+		t=new Thread(this,name);
+		System.out.println("\n Even:"+t);
+		t.start();}
+	public void run() {
+		int number=30;
+		System.out.println("Even upto 30:");
+		for(int i=1;i<=number;i++)
+			if(i%2==0)
+				System.out.print(i+"");
+		
+	}
+}
+public class FiboEven {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new Fibonacci("Fibonacci");
+		new Even("Even");
+		try {
+			Thread.sleep(10000);
+			}catch(InterruptedException e) {
+				System.out.println("\n main thread interrupted");}
+		System.out.println("\n main thread existing");
+		
+
+	}
+
+}
