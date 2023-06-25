4a493e714bdec312b96f887688f0d4bb6d18d532
ahmdmushfiq <115775322+ahmdmushfiq@users.noreply.github.com>
2023-06-25 20:00:54 +0530

    Add files via upload

---
+++ 03_mushfiq/11.Inheritance/Teacher.java
@@ -0,0 +1,18 @@
+package employeeTeach;
+
+class Teacher extends EmployeeT {
+	String department,subject;
+	Teacher(int empid, String name, float salary, String address,String department, String subject){
+		super(empid,name,salary,address);
+		this.department=department;
+		this.subject=subject;
+	}
+	public void display() {
+		System.out.println("teaher id: " +empid);
+		System.out.println("teaher name: " +name);
+		System.out.println("teaher salary: " +salary);
+		System.out.println("teaher address: " +address);
+		System.out.println("teaher department: " +department);
+		System.out.println("teaher subject: " +subject);
+	}
+}
