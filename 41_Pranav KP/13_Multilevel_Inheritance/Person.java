695505755e58ca6ac4ddc4a4e35a36604d083ced
Pranav <100040780+Pranav-3@users.noreply.github.com>
2023-06-25 22:57:24 +0530

    Add files via upload

---
+++ 41_Pranav KP/13_Multilevel_Inheritance/Person.java
@@ -0,0 +1,16 @@
+package PackA;
+
+public class Person {
+
+	 String name;
+     String gender;
+     String address;
+     int age;
+     Person(){}
+     Person(String name, String gender, String address,int age){
+         this.name = name;
+         this.gender = gender;
+         this.address = address;
+         this.age = age;}}
+
+
