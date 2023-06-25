55cf0631c7f19dc5a85e0fc68ba9d8dfe0dcb26e
fayizmhmd <115775598+fayizmhmd@users.noreply.github.com>
2023-06-25 20:11:58 +0530

    Add files via upload

---
+++ 37_Muhammed Fayiz K/17.UserDefinedPackage/figures/Rectangle.java
@@ -0,0 +1,8 @@
+package figures;
+
+public class Rectangle implements FigureRc{
+	private int length;int breadth;
+	public Rectangle(int l,int b) {length=l;breadth=b;}
+	public int perimeter() {return(2*(length+breadth));}
+	public int area() {return(length*breadth);}
+}
