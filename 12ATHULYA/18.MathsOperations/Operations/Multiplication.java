0e8332f3f542a40d7e7df46ca0352a24ea4e52ac
athulya2022 <115775630+athulya2022@users.noreply.github.com>
2023-06-26 11:18:39 +0530

    Add files via upload

---
+++ 12ATHULYA/18.MathsOperations/Operations/Multiplication.java
@@ -0,0 +1,8 @@
+package Operations;
+
+public class Multiplication {
+	private double num1,num2;
+	public Multiplication(double a,double b) {
+		num1=a; num2=b;}
+	public double mul() {return(num1*num2);}
+}
