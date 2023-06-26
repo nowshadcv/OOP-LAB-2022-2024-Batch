609c711becce266cf3b4df94004be1d529990622
juhanapa09 <115775860+juhanapa09@users.noreply.github.com>
2023-06-26 06:20:10 -0400

    Add files via upload

---
+++ 17_farhana m k/21.fibonacci/FiboEven.java
@@ -0,0 +1,14 @@
+package fibonacci;
+
+public class FiboEven {
+	public static void main(String[] args) { 
+		new fibonacci("Fibonacci"); 
+		new Even ("Even"); 
+		try { 
+		Thread.sleep(10000); 
+		} catch (InterruptedException e) { 
+		System.out.println("Main thread Interrupted");} 
+		System.out.println("\nMain thread exiting."); }} 
+
+
+
