017f270aa16179457cb465b199ad01f0a5b572bc
Fathimanihala <99164434+Fathimanihala@users.noreply.github.com>
2023-06-26 12:50:36 +0530

    Add files via upload

---
+++ 20_FATHIMA NIHALA/18.Arithmtc Package/Arithematic/Subtraction.java
@@ -0,0 +1,8 @@
+package Arithematic;
+
+public class Subtraction implements ArithmtcSub{
+	private int num1,num2;
+	public  Subtraction(int n1,int n2) {num1=n1;num2=n2;}
+	public int sub() {return num1-num2;}
+	
+}
