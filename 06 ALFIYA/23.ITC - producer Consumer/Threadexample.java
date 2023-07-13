69be520c77d474f8a6180a631356e1e2aa919954
Alfiyafaisalkt <115776061+Alfiyafaisalkt@users.noreply.github.com>
2023-07-13 16:01:37 +0530

    Add files via upload

---
+++ 06 ALFIYA/23.ITC - producer Consumer/Threadexample.java
@@ -0,0 +1,22 @@
+import java.util.LinkedList;
+public class Threadexample {
+
+	public static void main(String[] args) 
+	throws InterruptedException{
+		// TODO Auto-generated method stub
+		final PC pc = new PC();
+		Thread t1 = new Thread(new Runnable() {
+			public void run(){
+				try {pc.produce();
+				}catch (InterruptedException e) {
+					e.printStackTrace();}}});
+		
+		Thread t2 = new Thread(new Runnable() {
+			public void run(){
+				try {pc.consume();
+				}catch (InterruptedException e) {
+					e.printStackTrace();}}});
+		t1.start();       t2.start();
+		t1.join();        t2.join();}}
+	
+				
